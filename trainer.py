86c5d7d87c694c8317eef231e7b3e5251ffdfc1c
Wenliang_DAI <wenliang.dai.1995@gmail.com>
2020-05-12 14:22:17 +0800

    refine training prints and saves

--- trainer.py
+++ trainer.py
@@ -11,6 +11,27 @@ class Trainer():
         self.criterion = criterion
         self.optimizer = optimizer
         self.dataloaders = dataloaders
+        self.earlyStop = args['early_stop']
+
+        self.all_train_stats = []
+        self.all_valid_stats = []
+
+        self.prev_train_stats = [float('inf')] + [-float('inf')] * 5
+        self.prev_valid_stats = [float('inf')] + [-float('inf')] * 5
+
+        self.best_valid_stats = [float('inf')] + [-float('inf')] * 5
+        self.best_epoch = -1
+
+    def make_stat(self, prev, curr):
+        new_stats = []
+        for i in range(len(prev)):
+            if curr[i] > prev[i]:
+                new_stats.append(f'{curr[i]:.4f} \u2191')
+            elif curr[i] < prev[i]:
+                new_stats.append(f'{curr[i]:.4f} \u2193')
+            else:
+                new_stats.append(f'{curr[i]:.4f} -')
+        return new_stats
 
     def train(self):
         for epoch in range(1, self.args['epochs'] + 1):
@@ -20,9 +41,37 @@ class Trainer():
             train_stats = self.train_one_epoch()
             valid_stats = self.validate_one_epoch()
 
+            self.all_train_stats.append(train_stats)
+            self.all_valid_stats.append(valid_stats)
+
+            for i in range(len(valid_stats)):
+                if i == 0: # MAE
+                    if valid_stats[i] < self.best_valid_stats[i]:
+                        self.best_valid_stats[i] = valid_stats[i]
+                else:
+                    if valid_stats[i] > self.best_valid_stats[i]:
+                        self.best_valid_stats[i] = valid_stats[i]
+                        if i == 3: # Acc7
+                            self.earlyStop = self.args['early_stop']
+                    elif i == 3: # Acc7
+                        self.earlyStop -= 1
+
+            # For printing
+            train_stats_str = self.make_stat(self.prev_train_stats, train_stats)
+            valid_stats_str = self.make_stat(self.prev_valid_stats, valid_stats)
+
+            self.prev_train_stats = train_stats
+            self.prev_valid_stats = valid_stats
+
             headers = ['Phase', 'MAE', 'Acc2', 'Acc5', 'Acc7', 'F1', 'Corr']
-            print(tabulate([['Train', *train_stats], ['Valid', *valid_stats]], headers=headers))
+            print(tabulate([['Train', *train_stats_str], ['Valid', *valid_stats_str]], headers=headers))
             print()
+            # End printing
+
+            if self.earlyStop == 0:
+                print('Early stopping...')
+                break
+
 
     def train_one_epoch(self):
         self.model.train()
